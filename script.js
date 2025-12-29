8f6211fcd5a5846bb395908bba5487ee5f29d2b7
castawaypirate <apsikiotis@gmail.com>
2025-12-29 23:10:11 +0200

    filter posts with same title

--- script.js
+++ script.js
@@ -17,19 +17,38 @@ fetch("devlog.txt")
     let posts = text.split("@@@").slice(1);
     for (let post of posts) {
       let contents = post.split("#").slice(1);
-      let postObject = new Post(
-        contents[0].replace("Title: ", ""),
-        contents[2].replace("Body: ", ""),
-      );
-      postObject.addDate(contents[1].replace("Date: ", ""));
-      postObjects.push(postObject);
+      let title = contents[0].replace("Title: ", "");
+      let body = contents[2].replace("Body: ", "");
+      let date = contents[1].replace("Date: ", "");
+      let exists = false;
+      for (let postObject of postObjects) {
+        if (postObject.title === title) {
+          postObject.body += "\r\n" + body;
+          postObject.addDate(date);
+          exists = true;
+        }
+      }
+
+      if (!exists) {
+        let postObject = new Post(
+          contents[0].replace("Title: ", ""),
+          contents[2].replace("Body: ", ""),
+        );
+        postObject.addDate(contents[1].replace("Date: ", ""));
+        postObjects.push(postObject);
+      }
     }
+
+    console.log(postObjects);
 
     const postsContainer = document.querySelector("#posts-container");
     for (let post of postObjects) {
       const node = document.createElement("div");
       node.textContent = post.title;
       postsContainer.appendChild(node);
+      const para = document.createElement("p");
+      para.textContent = post.body;
+      postsContainer.appendChild(para);
     }
   })
   .catch((e) => console.error(e));
