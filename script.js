5068b7278243cb03f848277054f1e0cedc0f3f3c
castawaypirate <apsikiotis@gmail.com>
2025-12-30 19:24:00 +0200

    sorting and table

--- script.js
+++ script.js
@@ -17,38 +17,80 @@ fetch("devlog.txt")
     let posts = text.split("@@@").slice(1);
     for (let post of posts) {
       let contents = post.split("#").slice(1);
-      let title = contents[0].replace("Title: ", "");
-      let body = contents[2].replace("Body: ", "");
-      let date = contents[1].replace("Date: ", "");
+      let title = contents[0].replace("Title: ", "").replace(/\n/g, "");
+      let body = contents[2].replace("Body: ", "").replace(/\n/g, "<br>");
+      let date = contents[1].replace("Date: ", "").replace(/\n/g, "");
       let exists = false;
       for (let postObject of postObjects) {
         if (postObject.title === title) {
-          postObject.body += "\r\n" + body;
+          postObject.body += body;
           postObject.addDate(date);
           exists = true;
         }
       }
 
       if (!exists) {
-        let postObject = new Post(
-          contents[0].replace("Title: ", ""),
-          contents[2].replace("Body: ", ""),
-        );
-        postObject.addDate(contents[1].replace("Date: ", ""));
+        let postObject = new Post(title, body);
+        postObject.addDate(date);
         postObjects.push(postObject);
       }
     }
 
+    postObjects.sort((a, b) => a.dates[0] < b.dates[0]);
     console.log(postObjects);
 
+    // for (let post of postObjects) {
+    //   const node = document.createElement("div");
+    //   node.textContent = post.title;
+    //   postsContainer.appendChild(node);
+    //   const para = document.createElement("p");
+    //   para.innerHTML = `<div>${post.body}</div>`;
+    //   postsContainer.appendChild(para);
+    // }
+
+    const formatDate = (date) => {
+      return new Intl.DateTimeFormat("en-US", {
+        day: "numeric",
+        month: "short",
+        year: "numeric",
+      }).format(date);
+    };
+
     const postsContainer = document.querySelector("#posts-container");
-    for (let post of postObjects) {
-      const node = document.createElement("div");
-      node.textContent = post.title;
-      postsContainer.appendChild(node);
-      const para = document.createElement("p");
-      para.textContent = post.body;
-      postsContainer.appendChild(para);
-    }
+    // clean dashboard from previous posts
+    postsContainer.innerHTML = "";
+
+    const table = document.createElement("table");
+    table.className = "dashboard-table";
+    const tbody = document.createElement("tbody");
+    postObjects.forEach((post, index) => {
+      const tr = document.createElement("tr");
+      const numberCell = document.createElement("td");
+      const number = document.createElement("h3");
+      number.className = "number";
+      number.textContent = 1 + index + ". ";
+      numberCell.appendChild(number);
+      const postCell = document.createElement("td");
+
+      const postContainer = document.createElement("div");
+      postContainer.className = "post-container";
+
+      const postTitle = document.createElement("h3");
+      postTitle.className = "post-title";
+      postTitle.textContent = post.title;
+
+      const postDetails = document.createElement("div");
+      postDetails.className = "post-details";
+
+      postContainer.appendChild(postTitle);
+      postContainer.appendChild(postDetails);
+
+      postCell.appendChild(postContainer);
+      tr.appendChild(numberCell);
+      tr.appendChild(postCell);
+      tbody.appendChild(tr);
+    });
+    table.appendChild(tbody);
+    postsContainer.appendChild(table);
   })
   .catch((e) => console.error(e));
